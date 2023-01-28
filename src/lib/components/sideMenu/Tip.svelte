176ab87e31ef9c9dd42f9f8a06def12be8a80131
Jannis K <jannis.karanikis@gmail.com>
2023-01-28 16:17:11 +0100

    tip component

---
+++ src/lib/components/sideMenu/Tip.svelte
@@ -0,0 +1,18 @@
+<script context="module" lang="ts">
+	export type TipData = {
+		title: string;
+		content: string;
+		link: string | null;
+	};
+</script>
+
+<script lang="ts">
+	import { fly } from 'svelte/transition';
+
+	export let tipData: TipData;
+</script>
+
+<details class="bg-pink-400/70 p-4 my-4 rounded">
+	<summary class=""> {tipData.title}</summary>
+	<p class="font-mono text-xs my-4">{@html tipData.content}</p>
+</details>
