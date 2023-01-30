f74df5ca5e019a3311b22ca3cc870d1f1ad89b79
Jannis K <jannis.karanikis@gmail.com>
2023-01-30 09:49:30 +0100

    made header title link to home

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,6 +1,8 @@
 <div
 	class="w-full sticky top-0 border-b-2 m-0 bg-sky-400/40 rounded backdrop-hue-rotate-180 backdrop-blur-sm border-sky-600 p-4 text-2xl font-bold"
 >
-	<span class="text-pink-500 lg:ml-4">Automate </span>
-	<span class="text-sky-500">Me!</span>
+	<a href="/">
+		<span class="text-pink-500 lg:ml-4">Automate </span>
+		<span class="text-sky-500">Me!</span>
+	</a>
 </div>
