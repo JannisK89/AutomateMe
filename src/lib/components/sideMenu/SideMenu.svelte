bdcac496e91586ad6ceb94800d584819bf371746
Jannis K <jannis.karanikis@gmail.com>
2023-01-30 09:05:20 +0100

    side menu and button fixes for different resolutions

--- src/lib/components/sideMenu/SideMenu.svelte
+++ src/lib/components/sideMenu/SideMenu.svelte
@@ -18,14 +18,14 @@
 
 {#if $showSideMenu}
 	<aside
-		transition:fly={{ duration: 350, x: -500, opacity: 0.8, easing: quintOut }}
+		transition:fly={{ duration: 600, x: -2000, opacity: 0.8, easing: quintOut }}
 		class="bg-sky-400/40  lg:w-4/12
 		flex flex-col lg:mr-4 lg:fixed lg:top-0 lg:left-0 lg:overflow-y-auto lg:block inset-0 lg:min-h-screen shadow-inner lg:my-16"
 	>
 		<div class="m-6 lg:m-10 flex flex-col flex-wrap ">
 			<h1 class="text-2xl mb-2 font-semibold ">{menuData.header}</h1>
-			<p class="mb-4 lg:tracking-wide	font-extralight">
-				{menuData.content}
+			<p class="mb-4 lg:tracking-wide	font-light">
+				{@html menuData.content}
 			</p>
 			{#each tips as tip}
 				<Tip tipData={tip} />
--- src/lib/components/sideMenu/SideMenuButton.svelte
+++ src/lib/components/sideMenu/SideMenuButton.svelte
@@ -6,7 +6,7 @@
 
 <button
 	on:click={sideMenuToggle}
-	class="lg:left-1/3 border-2 border-sky-600/30 bg-sky-600/50 text-slate-100 rounded-r-lg hidden lg:block h-screen "
+	class="lg:left-1/3 border-2 border-sky-600/30 bg-sky-600/50 text-slate-100 rounded-r-lg hidden lg:block  h-[calc(100vh-4.2rem)]"
 	class:lg:fixed={$showSideMenu}>{$showSideMenu ? '<	' : '>'}</button
 >
 
