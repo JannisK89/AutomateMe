74a97290ec24a839c37bc833ec09573caa68e0ce
Jannis K <jannis.karanikis@gmail.com>
2023-02-05 12:33:46 +0100

    created draggable and droppable list components

---
+++ src/lib/components/DraggableList.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	export let itemDragList: string[];
+
+	const dragStartHandler = (
+		e: DragEvent & {
+			currentTarget: EventTarget & HTMLLIElement;
+		}
+	) => {
+		e.dataTransfer?.setData('text/plain', e.currentTarget.innerText);
+	};
+</script>
+
+<ul class="bg-slate-50 border p-2 rounded-xl flex flex-wrap min-w-full min-h-[4rem] justify-center">
+	{#each itemDragList as item}
+		<li class="m-1 border p-2" draggable="true" on:dragstart={(e) => dragStartHandler(e)}>
+			{item}
+		</li>
+	{/each}
+</ul>
---
+++ src/lib/components/DroppableList.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+	export let updateDragList: Function;
+	let itemDropList: string[] = [];
+
+	const dropHandler = (
+		e: DragEvent & {
+			currentTarget: EventTarget & HTMLUListElement;
+		}
+	) => {
+		if (e.dataTransfer !== null) {
+			const transferItem = e.dataTransfer.getData('text/plain');
+			itemDropList.push(transferItem);
+			itemDropList = itemDropList;
+
+			updateDragList(transferItem);
+		}
+	};
+
+	const dragOverHandler = (e: DragEvent) => {
+		if (e.dataTransfer !== null) {
+			e.dataTransfer.dropEffect = 'move';
+		}
+	};
+</script>
+
+<ul
+	class="bg-slate-50 border p-2 rounded-xl flex flex-wrap min-w-full min-h-[4rem] justify-center"
+	on:drop|preventDefault={(e) => dropHandler(e)}
+	on:dragover|preventDefault={(e) => dragOverHandler(e)}
+>
+	{#each itemDropList as item}
+		<li class="m-1 border p-2">{item}</li>
+	{/each}
+</ul>
